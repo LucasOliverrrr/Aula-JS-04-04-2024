81aba32435addb0c402518b20412a68407766ade
LucasOliverrrr <lucasouze@gmail.com>
2024-04-04 09:19:28 -0300

    So da "true" se todas as palavras comecam com A

--- teste.js
+++ teste.js
@@ -1,8 +1,11 @@
-const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+const nomes = ["Ana Maria", "Antonio", "ARodrigo", "Alex", "ACristina"]
 console.log(nomes);
 
 const apenasComA = nomes.filter((n) => n.startsWith("A"))
 console.log(apenasComA)
 
-const res = nomes.map((nome) => nome.charAt(0))
+const res = nomes.map((nome) => nome.charAt(0)) //0 é o posicionamento da letra
 console.log(res);
+
+const todosComecamComA = nomes.every((n) => n.startsWith("A"));
+console.log(todosComecamComA);
