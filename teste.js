666a8f7a22dd8011150ce4c04117a3d5a10ea35a
LucasOliverrrr <lucasouze@gmail.com>
2024-04-04 09:15:43 -0300

    Pegar a primeira letra de cada palavra

--- teste.js
+++ teste.js
@@ -2,4 +2,7 @@ const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
 console.log(nomes);
 
 const apenasComA = nomes.filter((n) => n.startsWith("A"))
-console.log(apenasComA);
+console.log(apenasComA)
+
+const res = nomes.map((nome) => nome.charAt(0))
+console.log(res);
